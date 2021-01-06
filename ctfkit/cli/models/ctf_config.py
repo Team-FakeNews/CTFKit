6a00f4e90d2753d7d81b440334dd93e3d1b25088
Adam LE BON <lebon.adam@gmail.com>
2021-01-06 23:14:34 +0100

    Correction d'erreurs
    [UPT] Version de python dans le Pipfile
    [UPT] Ordre des commit dans ctf_config.py
    PS: maintenant ca fonctionne promis

--- ctfkit/cli/models/ctf_config.py
+++ ctfkit/cli/models/ctf_config.py
@@ -5,16 +5,15 @@ import trafaret as t
 import trafaret_config
 from typing import Optional
 
-from ctfkit.cli.models.HostingProvider import HOSTING_PROVIDER
-from ctfkit.cli.models.HostingEnvironment import HOSTING_ENVIRONMENT
 from ctfkit.utility import enum_to_regex
+from .hosting_environment import HOSTING_ENVIRONMENT
+from .hosting_provider import HOSTING_PROVIDER
 
 CONFIG_MODEL = t.Dict({
     t.Key("kind"): t.Regexp(r"^ctf$"),
     t.Key("name"): t.String(),
     t.Key("challenges"): t.List(t.String()),
     t.Key("deployments", default=[]): t.List(t.Dict({
-        # t.Key("environment"): t.Enum(HOSTING_ENVIRONMENT),
         t.Key("environment"): t.Regexp(enum_to_regex(HOSTING_ENVIRONMENT)),
         t.Key("provider"): t.Regexp(enum_to_regex(HOSTING_PROVIDER)),
     }))
