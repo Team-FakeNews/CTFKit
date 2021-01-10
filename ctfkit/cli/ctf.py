a43ba8d13ee864f1d7b779d17b3d470ab5e18608
Adam LE BON <lebon.adam@gmail.com>
2021-01-10 02:22:11 +0100

    Using context to pass ctf configuration to child commands

--- ctfkit/cli/ctf.py
+++ ctfkit/cli/ctf.py
@@ -1,13 +1,20 @@
 from pprint import pformat, pprint
+from typing import Callable
+from click.core import Context
+
 from ctfkit.utility import ConfigLoader
 import click
 from ctfkit.models import CtfConfig
 
 
+pass_config = click.make_pass_decorator(CtfConfig)
+
+
 @click.group()
 @click.option("--config", type=ConfigLoader(CtfConfig), default="ctf.config.yaml")
-def cli(config):
-    pprint(config)
+@click.pass_context
+def cli(context: Context, config: CtfConfig):
+    context.obj = config
 
 
 @cli.command('init')
@@ -15,6 +22,8 @@ def init():
     pass
 
 
-@cli.command('genconfig')
-def generate_config():
-    pass
+@cli.command('plan')
+@click.argument('environment', required=True)
+@pass_config
+def plan(config: CtfConfig, environment: str):
+    pprint(config)
