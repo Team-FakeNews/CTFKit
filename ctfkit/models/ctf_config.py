6ab599e07ae71d62606e90afa1bd00d243db392b
Adam LE BON <lebon.adam@gmail.com>
2021-01-10 01:56:08 +0100

    Switched from trafaret to marshmallow and dataclasses

--- ctfkit/models/ctf_config.py
+++ ctfkit/models/ctf_config.py
@@ -1,5 +1,6 @@
+from pprint import pformat, pprint
 from typing import Dict, List, Optional
-from attr import dataclass
+from dataclasses import dataclass, field
 from yaml import load, SafeLoader
 
 from click import Path
@@ -12,48 +13,39 @@ from .hosting_environment import HOSTING_ENVIRONMENT
 from .hosting_provider import HOSTING_PROVIDER
 
 
-# CONFIG_MODEL = t.Dict({
-#     t.Key("kind"): t.Regexp(r"^ctf$"),
-#     t.Key("name"): t.String(),
-#     t.Key("challenges"): t.List(t.String()),
-#     t.Key("deployments", default=[]): t.List(t.Dict({
-#         t.Key("environment"): t.Regexp(enum_to_regex(HOSTING_ENVIRONMENT)),
-#         t.Key("provider"): t.Regexp(enum_to_regex(HOSTING_PROVIDER)),
-#         t.Key("cluster"): t.Dict({
-#             t.Key("machine_type"): t.String(),
-#             t.Key("node_count"): t.Int(),
-#         }),
-#     })),
-# })
+@dataclass
+class ClusterConfig:
+
+    machine_type: str = 'n1-standard-4'
+    node_count: int = 1
+
+    def __repr__(self) -> str:
+        return pformat(vars(self))
+
+
+@dataclass
+class DeploymentConfig:
+
+    environment: HOSTING_ENVIRONMENT = field(
+        default=None, metadata={"by_value": True})
+    provider: HOSTING_PROVIDER = field(
+        default=None, metadata={"by_value": True})
+    cluster: ClusterConfig = ClusterConfig()
+
+    def __repr__(self) -> str:
+        return pformat(vars(self), indent=4)
 
 
 @dataclass
 class CtfConfig():
 
-    @dataclass
-    class DeploymentConfig:
-
-        @dataclass
-        class ClusterConfig:
-            machine_type: str = 'n1-standard-4'
-            node_count: int = 1
-
-        environment: HOSTING_ENVIRONMENT = HOSTING_ENVIRONMENT.TESTING
-        provider: HOSTING_PROVIDER = HOSTING_PROVIDER.GCP
-        cluster: ClusterConfig = ClusterConfig()
-
     kind: str
     name: str
-    challenge: List[str] = []
-    deployments: List[DeploymentConfig] = []
+    challenges: List[str] = field(default_factory=list)
+    deployments: List[DeploymentConfig] = field(default_factory=list)
 
     def getSlug(self) -> str:
         slugify(self.name)
 
-
-
-        # try:
-        #     return trafaret_config.read_and_validate(config_file, CONFIG_MODEL)
-
-        # except trafaret_config.ConfigError as e:
-        #     self.fail("\n".join(str(err) for err in e.errors))
+    def __repr__(self) -> str:
+        return pformat(vars(self))
