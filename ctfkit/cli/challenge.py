08958e1dfff9d40cea424ad362dc02096f2d149e
0xShiroKuma <0xshirokuma@protonmail.ch>
2021-01-13 19:34:31 +0100

    Summary of the push
    [UPT] run() function
    [UPT] stop() function
    [ADD] PEP8 compliance
    
    [DEL] Traceback printed
    [ADD] Docstrings

--- ctfkit/cli/challenge.py
+++ ctfkit/cli/challenge.py
@@ -1,92 +1,102 @@
 import click
 import docker
 from yaspin import yaspin
-import time
-import traceback
+
 
 @click.group()
 def cli():
     pass
 
+
 @cli.command('init')
 def init():
     pass
 
-@cli.command('run_challenge')
-@click.argument('challenge', required=True)
-def run_challenge(challenge):
-    #Check if the given challenge exists
 
+@cli.command('run')
+@click.argument('challenge', required=True)
+def run(challenge):
+    """Run a challenge inside a Docker container.
+    For the moment this method don't add specific parameters to the container (such as port) but it will when a challenge object will be available.
+    First a Docker image is build using the Dockerfile located in the challenge's directory and then run a Docker container with the builded image.
+
+    :param challenge: The name of the challenge to run
+    :type file: str
+    :return: None
+    :rtype: None
+    """
+    """TODO:
+        Use a challenge object to get the information related to the challenge path, parameters like needed ports or volumes)
+    """
     try:
-        #instance of the docker env
+        # Instance of the docker env
         client = docker.from_env()
     except:
             print("\n❌ Error, please check that Docker is installed and that your user has the proper rights to run it. :\n")
-            traceback.print_exc()
-    #build the docker image
-    #tmp_challenge_path TMP !!!!
+    # Build the docker image
     tmp_challenge_path = "./"+challenge
     image_name = "ctfkit:"+challenge
     with yaspin(text="Starting challenge "+challenge, color="cyan") as sp:
         try:
-            client.images.build(path=tmp_challenge_path, rm=True, forcerm=True, tag=image_name)
-            time.sleep(0.5)
+            client.images.build(
+                path=tmp_challenge_path, 
+                rm=True, forcerm=True, 
+                tag=image_name)
             sp.write("✔ Image building")
         except:
-            time.sleep(0.5)
             print("\n❌ Error while building Docker Image, please check if a Dockerfile exists and if it's correct :\n")
-            traceback.print_exc()
             exit(1)
         try:
-            client.containers.run(image_name, auto_remove=True, detach=True, name="ctfkit_"+challenge)
+            client.containers.run(
+                image_name, 
+                auto_remove=True, 
+                detach=True, 
+                name="ctfkit_"+challenge)
             sp.ok("✔")
         except:
-            time.sleep(0.5)
             print("\n❌ Error, unable to run a Docker container based on the image "+image_name+" :\n")
-            traceback.print_exc()
             exit(1)
 
-@cli.command('stop_challenge')
-@click.argument('challenge', required=True)
-def stop_challenge(challenge):
-    #Check if the given challenge exists
-    #Check if the given challenge is running
 
+@cli.command('stop')
+@click.argument('challenge', required=True)
+def stop(challenge):
+    """Stop a challenge running inside a Docker container.
+    First we stop the container using his name, then we delete the related Docker image.
+
+    :param challenge: The name of the running challenge.
+    :type file: str
+    :return: None
+    :rtype: None
+    """
     try:
-        #instance of the docker env
+        # Instance of the docker env
         client = docker.from_env()
     except:
             print("\n❌ Error, please check that Docker is installed and that your user has the proper rights to run it. :\n")
-            traceback.print_exc()
             exit(1)
     container_name = "ctfkit_"+challenge
-    with yaspin(text="Starting challenge "+challenge, color="cyan") as sp:
-        #Get the container related to the challenge
+    with yaspin(text="Stopping challenge "+challenge, color="cyan") as sp:
+        # Get the container related to the challenge
         try:
             container = client.containers.get(container_name)
         except:
-            time.sleep(0.5)
-            print("\n❌ Error, please check that the supplied challenge is running :\n")
-            traceback.print_exc()
+            print("\n❌ Error, please check that the supplied challenge is running : "+container_name)
             exit(1)
-        #Stop the container
+        # Stop the container
         try:
             container.stop()
         except:
-            time.sleep(0.5)
             print("\n❌ Error, unable to stop the Docker container :\n")
-            traceback.print_exc()
             exit(1)
-        time.sleep(0.5)
         sp.write("✔ Challenge stopping")
-        #remove the related image
+        # Remove the related image
         try:
-            client.images.remove(image=container.image.id, force=True)
+            client.images.remove(
+                image=container.image.id, 
+                force=True)
         except:
-            time.sleep(0.5)
             print("\n❌ Error, unable to remove the Docker image related to the challenge :\n")
-            traceback.print_exc()
             exit(1)
-        time.sleep(0.5)
         sp.write("✔ Image removing")
         sp.ok("✔")
