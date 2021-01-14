cf95079ab51b38e3f8004fad5b47ae30a69275a1
Adam LE BON <adam.lebon@bystamp.com>
2021-01-14 16:53:37 +0100

    Issue related to merge conflict

--- tests/test_create_challenge.py
+++ tests/test_create_challenge.py
@@ -6,12 +6,12 @@ from ctfkit.cli.challenge import *
 
 class TestCreateChallengeFunctions(unittest.TestCase):
 
-    self.challenge_name_valid = 'my-new-challenge'
-    self.challenge_name_invalid = 'mÿ néw CHallenge'
-    self.challenge_url_valid = 'http://my-git.com/my-ctfkit-challenge'
-    self.challenge_url_invalid = '///my-git/my-ctfkit-challenge'
-    self.challenge_config_valid = os.path.join('example', 'challs', '01-test', 'challenge.yml')
-    self.challenge_valid = Challenge("test_challenge", "desc", 1, "cat", "author", False, False)
+    challenge_name_valid = 'my-new-challenge'
+    challenge_name_invalid = 'mÿ néw CHallenge'
+    challenge_url_valid = 'http://my-git.com/my-ctfkit-challenge'
+    challenge_url_invalid = '///my-git/my-ctfkit-challenge'
+    challenge_config_valid = os.path.join('example', 'challs', '01-test', 'challenge.yml')
+    challenge_valid = Challenge("test_challenge", "desc", 1, "cat", "author", False, False)
 
     # Check that a challenge is not created if the name is not valid, and vice versa
     def test_new_challenge(self):
