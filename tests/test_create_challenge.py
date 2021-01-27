17635e03739f70a204ed9479a894a0c50493c66f
0xNinja-win <57400445+OxNinja@users.noreply.github.com>
2021-01-27 18:19:15 +0100

    Finally implemented ConfigLoader within the Challenge class
    [UPT] /ctfkit/challenge/challenge.py: Challenge class uses only one attribute: `self.config` which represents its configuration (such as `self.config.author`, `self.config.container`...)
    [ADD] /ctfkit/models/challenge_config.py: Added `has_files()` and `has_container()` methods
    [UPT] Better commenting (PEP8) for some files in the commit
    
    [UPT] /tests/test_create_challenge.py: updated the test with a working config

--- tests/test_create_challenge.py
+++ tests/test_create_challenge.py
@@ -3,6 +3,9 @@ import os
 
 from ctfkit.challenge import Challenge
 from ctfkit.cli.challenge import *
+from ctfkit.models import challenge_config
+from ctfkit.utility import ConfigLoader
+
 
 class TestCreateChallengeFunctions(unittest.TestCase):
 
@@ -11,9 +14,11 @@ class TestCreateChallengeFunctions(unittest.TestCase):
     challenge_url_valid = 'http://my-git.com/my-ctfkit-challenge'
     challenge_url_invalid = '///my-git/my-ctfkit-challenge'
     challenge_config_valid = os.path.join('example', 'challs', '01-test',
-    'challenge.yml')
-    challenge_valid = Challenge("test_challenge", "desc", 1, "cat", "author",
-    False, False)
+                                          'challenge.yml')
+    challenge_valid = Challenge(config="""{'name':'chall',
+    'description':'blablabla','points':10,'category':'pwn',
+    'author':'0xLaPoutre','files':['chall.jpg'],
+    'container':[{'proto':'tcp','port':1337},{'proto':'udp','port':71337}]""")
 
     # Check that a challenge is not created if the name is not valid, and vice
     # versa
@@ -31,8 +36,9 @@ class TestCreateChallengeFunctions(unittest.TestCase):
     # config file
     def test_challenge_from_yaml(self):
         self.assertTrue(
-            type(Challenge.from_yaml(self.challenge_config_valid)) == 
+            type(Challenge.from_yaml(self.challenge_config_valid)) ==
             type(self.challenge_valid))
+
 
 if __name__ == '__main__':
     unittest.main()
