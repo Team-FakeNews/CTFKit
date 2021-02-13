02222d62f54eb47f1b1752769ee2b3a94e804dc2
Adam LE BON <lebon.adam@gmail.com>
2021-02-13 19:03:28 +0100

    Fixed "ctf init" unit testing

--- tests/test_create_ctf.py
+++ tests/test_create_ctf.py
@@ -1,6 +1,6 @@
 import unittest
 from os import system
-from os.path import isdir, isfile, join
+from os.path import isdir, join
 
 from ctfkit.cli.ctf import *
 from ctfkit.utility import get_current_path
@@ -15,12 +15,12 @@ class TestCreateCTF(unittest.TestCase):
     # Check that a CTF is not created if the name is not valid, and vice versa
     def test_new_ctf(self):
         # Check for invalid name
-        system(f"ctfkit ctf init -n {ctf_name_invalid}")
-        self.assertFalse(isdir(join(path, ctf_name_invalid)))
+        system(f"ctfkit ctf init -p gcp -n {self.ctf_name_invalid}")
+        self.assertFalse(isdir(join(self.path, self.ctf_name_invalid)))
 
         # Check for valid name
-        system(f"ctfkit ctf init -n {ctf_name_valid}")
-        self.assertTrue(isdir(join(path, ctf_name_valid)))
+        system(f"ctfkit ctf init -p gcp -n {self.ctf_name_valid}")
+        self.assertTrue(isdir(join(self.path, self.ctf_name_valid)))
 
 
 if __name__ == '__main__':
