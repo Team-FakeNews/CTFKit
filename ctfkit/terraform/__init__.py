ffcb1a8055cf8496d7ded8e068a552cc3c9a7472
Adam LE BON <lebon.adam@gmail.com>
2021-02-16 00:01:38 +0100

    Fixed typing issue in terraform module

--- ctfkit/terraform/__init__.py
+++ ctfkit/terraform/__init__.py
@@ -120,7 +120,7 @@ class CtfStack(TerraformStack):
             with open(self.deployment_config.gcp.credentials_file, 'r') as credentials:
                 GoogleProvider(
                     self,
-                    HostingProvider.GCP,
+                    HostingProvider.GCP.value,
                     credentials=credentials.read(),
                     project=self.deployment_config.gcp.project,
                     region=self.deployment_config.gcp.region,
